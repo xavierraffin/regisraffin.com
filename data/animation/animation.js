3e4b0cf7956b4eaffc2c47b6fe7599ea70b41e57
Xavier Raffin <xavier.raffin@gmail.com>
2014-01-05 23:39:33 +0100

    image

--- data/animation/animation.js
+++ data/animation/animation.js
@@ -1,17 +1,42 @@
 var videos = {
         "movie1": {
-            "head": "Ryhtm n White",
-            "desc": "Ryhtm n White (3mn44) / 2012<br>Director: Régis Raffin<br>With Jean Thamié, Régis Raffin<br>Production 15 Donadieu &<br>Maneki Lab<br>Producer : Michel Brissaud",
-            "vimeocode": 10347431
+            "head": "louis vuitton",
+            "desc": "the little groom is<br>fighting in the arena<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
+            "vimeocode": 82081351
         },
         "movie2": {
             "head": "Louis Vuitton",
-            "desc": "blabla<br>blibli<br>bloblo",
-            "vimeocode": 82103918
+            "desc": "the little groom in<br>duel in the dust<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
+            "vimeocode": 82103917
         },
         "movie3": {
-            "head": "Michael Jackson",
-            "desc": "2013<br>Producer : God Himself",
+            "head": "louis vuitton",
+            "desc": "the little groom<br>falls in love in roma<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
+            "vimeocode": 82192852
+        },
+        "movie4": {
+            "head": "cinema",
+            "desc": "main title<br>la confrere des larmes<br>Director: Régis raffin<br>Production: Maneki Lab / Full house<br>2013",
+            "vimeocode": 82281952
+        },
+        "movie5": {
+            "head": "dyson",
+            "desc": "l'affaire airblade<br>Director: Régis raffin<br>drawings : marc riou<br>2010",
             "vimeocode": 82114225
-        }
+        },
+        "movie6": {
+            "head": "bnp paribas",
+            "desc": "smart start<br>Director: Régis raffin<br>Production: Maneki Lab<br>2012",
+            "vimeocode": 82103918
+        },
+        "movie7": {
+            "head": "louis vuitton",
+            "desc": "the little groom is<br>swinging in the clouds<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
+            "vimeocode": 82103920
+        },
+        "movie8": {
+            "head": "louis vuitton",
+            "desc": "the little groom<br>in marina bay<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
+            "vimeocode": 82192854
+
     };
