cb7e5925a61c583b33e3872a905585277ac053c8
Xavier Raffin <xavierraffin@gmail.com>
2014-01-05 18:30:56 +0100

    First dynamic version

---
+++ data/animation/animation.js
@@ -0,0 +1,17 @@
+var videos = {
+        "movie1": {
+            "head": "Ryhtm n White",
+            "desc": "Ryhtm n White (3mn44) / 2012<br>Director: Régis Raffin<br>With Jean Thamié, Régis Raffin<br>Production 15 Donadieu &<br>Maneki Lab<br>Producer : Michel Brissaud",
+            "vimeocode": 10347431
+        },
+        "movie2": {
+            "head": "Louis Vuitton",
+            "desc": "blabla<br>blibli<br>bloblo",
+            "vimeocode": 82103918
+        },
+        "movie3": {
+            "head": "Michael Jackson",
+            "desc": "2013<br>Producer : God Himself",
+            "vimeocode": 82114225
+        }
+    };
---
+++ videoLoader.js
@@ -0,0 +1,13 @@
+function loadVideo(movie)
+{
+  video = $(".video");
+  $("#vimeo").attr('src','//player.vimeo.com/video/'+videos[movie].vimeocode);
+  $(video).css("display","inline-block");
+  
+  legend = $(".legend");
+  legendCustomer = $(".legend .customer");
+  legendDesc = $(".legend .desc");
+  $(legendCustomer).html(videos[movie].head);
+  $(legendDesc).html(videos[movie].desc);
+  $(legend).css("display","inline-block");
+}
