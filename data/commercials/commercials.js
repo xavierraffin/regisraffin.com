ceba0e1ccbc92fb9b653eac173bef0a060b11002
Xavier Raffin <xavier.raffin@gmail.com>
2014-01-05 23:57:53 +0100

    js

---
+++ data/commercials/commercials.js
@@ -0,0 +1,27 @@
+var videos = {
+        "movie1": {
+            "head": "bruel/marin",
+            "desc": "le concert<br>Director: Régis Raffin<br>Production: charlotte marin<br>2010",
+            "vimeocode": 18087560
+        },
+        "movie2": {
+            "head": "lee cooper",
+            "desc": "the fine english denim company<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
+            "vimeocode": 4689031
+        },
+        "movie3": {
+            "head": "greenpeace",
+            "desc": "ultimatum climatique<br>Director: Régis Raffin<br>Production: greenpeace<br>2009",
+            "vimeocode": 9802411
+        },
+        "movie4": {
+            "head": "lee cooper",
+            "desc": "originals<br>Director: Régis raffin<br>Production: lee cooper<br>2013",
+            "vimeocode": 4724243
+        },
+        "movie5": {
+            "head": "france 4",
+            "desc": "5 fruits et légumes par jours<br>Director: Régis raffin<br>production: eden<br>2011",
+            "vimeocode": 82778494
+
+    };
