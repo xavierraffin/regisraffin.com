3b7bd2c6a4035c668969620d1af4834d29714f27
Xavier Raffin <xavier.raffin@gmail.com>
2014-01-06 10:30:58 +0100

    js

---
+++ data/music-videos/music-videos.js
@@ -0,0 +1,42 @@
+var videos = {
+        "movie1": {
+            "head": "eric mcfadden",
+            "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
+            "vimeocode": 10347431
+        },
+        "movie2": {
+            "head": "marner brown",
+            "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
+            "vimeocode": 4715443
+        },
+        "movie3": {
+            "head": "charlotte marin",
+            "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
+            "vimeocode": 82192852
+        },
+        "movie4": {
+            "head": "kish mauve",
+            "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
+            "vimeocode": 4683162
+        },
+        "movie5": {
+            "head": "little big boy",
+            "desc": "rhythm n white<br>Director: Régis raffin<br>production: 15 donadieu/maneki lab<br>2012",
+            "vimeocode": 45399703
+        },
+        "movie6": {
+            "head": "albert de paname",
+            "desc": "tahiti<br>Director: Régis raffin<br>Production: elegangz<br>2008",
+            "vimeocode": 82103918
+        },
+        "movie7": {
+            "head": "koritni",
+            "desc": "heaven again<br>Director: Régis Raffin<br>Production: bad reputation<br>2008",
+            "vimeocode": 4979121
+        },
+        "movie8": {
+            "head": "elliott 1er",
+            "desc": "la pose<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
+            "vimeocode": 4690872
+
+    };
