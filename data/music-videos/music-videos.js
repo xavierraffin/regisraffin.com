38362ea89277a3f2badd697b298e557b2dc50957
Xavier Raffin <xavier.raffin@gmail.com>
2014-10-08 11:45:50 +0200

    new clip
    bien

--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -1,40 +1,45 @@
 var videos = {
         "movie1": {
+            "head": "republic of rock n roll",
+            "desc": "hipsters must die<br>Director: Régis Raffin<br>Production: Feerkins Prod<br>2014",
+            "vimeocode": 105849120
+        },
+	"movie2": {
             "head": "eric mcfadden",
             "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
             "vimeocode": 10347431
         },
-        "movie2": {
+        "movie3": {
             "head": "marner brown",
             "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4715443
         },
-        "movie3": {
+        "movie4": {
             "head": "charlotte marin",
             "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
             "vimeocode": 9802972
         },
-        "movie4": {
+        "movie5": {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
         },
-        "movie5": {
+        "movie6": {
             "head": "little big boy",
             "desc": "rhythm n white<br>Director: Régis raffin<br>production: 15 donadieu/maneki lab<br>2012",
             "vimeocode": 45399703
         },
-        "movie6": {
+        "movie7": {
             "head": "albert de paname",
             "desc": "tahiti<br>Director: Régis raffin<br>Production: elegangz<br>2008",
             "vimeocode": 4662187
         },
-        "movie7": {
+        "movie8": {
             "head": "koritni",
             "desc": "heaven again<br>Director: Régis Raffin<br>Production: bad reputation<br>2008",
             "vimeocode": 4979121
         },
-        "movie8": {
+        "movie9": {
             "head": "elliott 1er",
             "desc": "la pose<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 4690872
