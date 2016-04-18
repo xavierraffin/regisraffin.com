f8c4fc42a6bfd7753b2ae5bd6a69707f70cabf81
Xavier Raffin <xavier.raffin@gmail.com>
2016-04-18 13:12:12 +0200

    veggies

--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -1,45 +1,55 @@
 var videos = {
-        "movie1": {
+             "movie1": {
+            "head": "ror",
+            "desc": "veggies can kill<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
+            "vimeocode": 161273783
+        },
+         "movie2": {
+            "head": "ror",
+            "desc": "making of<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
+            "vimeocode": 163225802
+        },
+        "movie3": {
             "head": "ror",
             "desc": "hipsters must die<br>Director: Régis Raffin<br>Production: Feerkins Prod<br>2014",
             "vimeocode": 105849120
         },
-	"movie2": {
+	"movie4": {
             "head": "eric mcfadden",
             "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
             "vimeocode": 10347431
         },
-        "movie3": {
+        "movie5": {
             "head": "marner brown",
             "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4715443
         },
-        "movie4": {
+        "movie6": {
             "head": "charlotte marin",
             "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
             "vimeocode": 9802972
         },
-        "movie5": {
+        "movie7": {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
         },
-        "movie6": {
+        "movie8": {
             "head": "little big boy",
             "desc": "rhythm n white<br>Director: Régis raffin<br>production: 15 donadieu/maneki lab<br>2012",
             "vimeocode": 45399703
         },
-        "movie7": {
+        "movie9": {
             "head": "albert de paname",
             "desc": "tahiti<br>Director: Régis raffin<br>Production: elegangz<br>2008",
             "vimeocode": 4662187
         },
-        "movie8": {
+        "movie10": {
             "head": "koritni",
             "desc": "heaven again<br>Director: Régis Raffin<br>Production: bad reputation<br>2008",
             "vimeocode": 4979121
         },
-        "movie9": {
+        "movie11": {
             "head": "elliott 1er",
             "desc": "la pose<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 4690872
