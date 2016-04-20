73d28c5ae351b49172a1b8b8cd6b5d1956c72c5a
Xavier Raffin <xavier.raffin@gmail.com>
2016-04-20 12:11:31 +0200

    yeah 3

--- data/animation/animation.js
+++ data/animation/animation.js
@@ -15,33 +15,13 @@ var videos = {
             "vimeocode": 82103917
         },
         "movie4": {
-            "head": "louis vuitton",
-            "desc": "the little groom<br>falls in love in roma<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
-            "vimeocode": 82192852
-        },
-        "movie5": {
             "head": "cinema",
             "desc": "main title<br>la confrere des larmes<br>Director: Régis raffin<br>Production: Maneki Lab / Full house<br>2013",
             "vimeocode": 82281952
         },
-        "movie6": {
-            "head": "dyson",
-            "desc": "l'affaire airblade<br>Director: Régis raffin<br>drawings : marc riou<br>2010",
-            "vimeocode": 82114225
-        },
-        "movie7": {
-            "head": "bnp paribas",
-            "desc": "smart start<br>Director: Régis raffin<br>Production: Maneki Lab<br>2012",
-            "vimeocode": 82103918
-        },
-        "movie8": {
+        "movie5": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>swinging in the clouds<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 82103920
-        },
-        "movie9": {
-            "head": "louis vuitton",
-            "desc": "the little groom<br>in marina bay<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
-            "vimeocode": 82192854
         }
     };
