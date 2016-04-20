822cdd8a56e4627d3d402449dcf3abed75d32156
Xavier Raffin <xavier.raffin@gmail.com>
2016-04-20 11:48:30 +0200

    yeah

--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -5,7 +5,7 @@ var videos = {
             "vimeocode": 161273783
         },
          "movie2": {
-            "head": "ror",
+            "head": "making of",
             "desc": "making of<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
             "vimeocode": 163225802
         },
@@ -33,25 +33,5 @@ var videos = {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
-        },
-        "movie8": {
-            "head": "little big boy",
-            "desc": "rhythm n white<br>Director: Régis raffin<br>production: 15 donadieu/maneki lab<br>2012",
-            "vimeocode": 45399703
-        },
-        "movie9": {
-            "head": "albert de paname",
-            "desc": "tahiti<br>Director: Régis raffin<br>Production: elegangz<br>2008",
-            "vimeocode": 4662187
-        },
-        "movie10": {
-            "head": "koritni",
-            "desc": "heaven again<br>Director: Régis Raffin<br>Production: bad reputation<br>2008",
-            "vimeocode": 4979121
-        },
-        "movie11": {
-            "head": "elliott 1er",
-            "desc": "la pose<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
-            "vimeocode": 4690872
         }
     };
