1c378bbedcfacbf442732c97dc5a929dd3095c7e
Xavier Raffin <xavier.raffin@gmail.com>
2017-03-23 21:38:20 +0100

    new fiction
    new fiction

--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -14,22 +14,32 @@ var videos = {
             "desc": "hipsters must die<br>Director: Régis Raffin<br>Production: Feerkins Prod<br>2014",
             "vimeocode": 105849120
         },
-	"movie4": {
+        	"movie4": {
+            "head": "archifau",
+            "desc": "chirac<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2017",
+            "vimeocode": 209723372
+        },
+         "movie5": {
+            "head": "archifau",
+            "desc": "fillon<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2016",
+            "vimeocode": 209721876
+        },     
+	"movie6": {
             "head": "eric mcfadden",
             "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
             "vimeocode": 10347431
         },
-        "movie5": {
+        "movie7": {
             "head": "marner brown",
             "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4715443
         },
-        "movie6": {
+        "movie8": {
             "head": "charlotte marin",
             "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
             "vimeocode": 9802972
         },
-        "movie7": {
+        "movie9": {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
