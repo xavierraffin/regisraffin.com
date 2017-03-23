5a419828355dfc3ca1cc007cd7695d9776a53fa6
Xavier Raffin <xavier.raffin@gmail.com>
2017-03-23 21:52:29 +0100

    new clip
    new clip

--- data/fiction/fiction.js
+++ data/fiction/fiction.js
@@ -1,40 +1,34 @@
 var videos = {
 		"movie1": {
             "head": "archifau",
-            "desc": "kim kardashian<br>Director: Régis Raffin<br>DOP : Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2016",
+            "desc": "kim kardashian<br>Director: Régis Raffin<br>DOP: Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2016",
             "vimeocode": 209725113
               },
-    
    	  "movie2": {
             "head": "archifau",
             "desc": "julie depardieu<br>Director: Régis Raffin<br>Prod : Flab/Canal+<br>2016",
             "vimeocode": 209726967
-             },
-            
+             },            
     	 "movie3": {
             "head": "archifau",
-            "desc": "hug day<br>Director: Régis Raffin<br>DOP : Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2017",
+            "desc": "hug day<br>Director: Régis Raffin<br>DOP: Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2017",
             "vimeocode": 209724120
              },            
-
     	 "movie4": {
             "head": "archifau",
-            "desc": "highlander<br>Director: Régis Raffin<br>DOP : Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2017",
+            "desc": "highlander<br>Director: Régis Raffin<br>DOP: Rodolphe Souvaret<br>Prod : Flab/Canal+<br>2017",
             "vimeocode": 209722750
-             },  
-            
+             },              
 		"movie5": {
             "head": "archifau",
-            "desc": "christmas<br>Director: Régis Raffin<br>DOP : gregory bar<br>Prod : Flab/Canal+<br>2016",
+            "desc": "christmas<br>Director: Régis Raffin<br>DOP: gregory bar<br>Prod : Flab/Canal+<br>2016",
             "vimeocode": 209725931
              },  
-
 		"movie6": {
             "head": "bruel/marin",
             "desc": "le concert<br>Director: Régis Raffin<br>Production: charlotte marin<br>2010",
             "vimeocode": 18087560
-              },
-              
+              },              
         "movie7": {
             "head": "regis raffin",
             "desc": "denis le zombie<br>Director: Régis Raffin<br>Image: eddy briere/williams bonbon<br>2010",
