ddb25d153124f4e1374305ebd412544024c846b1
Xavier Raffin <xavier.raffin@gmail.com>
2018-04-13 16:13:21 +0200

    yes

--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -1,45 +1,50 @@
 var videos = {
-             "movie1": {
+			"movie1": {
+            "head": "pershanoush",
+            "desc": "mesmerizing<br>Director: Régis Raffin<br>Production: Gunsmoke<br>2016",
+            "vimeocode": 262004148
+        },
+             "movie2": {
             "head": "ror",
             "desc": "veggies can kill<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
             "vimeocode": 161273783
         },
-         "movie2": {
+         "movie3": {
             "head": "making of",
             "desc": "making of<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
             "vimeocode": 163225802
         },
-        "movie3": {
+        "movie4": {
             "head": "ror",
             "desc": "hipsters must die<br>Director: Régis Raffin<br>Production: Feerkins Prod<br>2014",
             "vimeocode": 105849120
         },
-        	"movie4": {
+        	"movie5": {
             "head": "archifau",
             "desc": "chirac<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2017",
             "vimeocode": 209723372
         },
-         "movie5": {
+         "movie6": {
             "head": "archifau",
             "desc": "fillon<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2016",
             "vimeocode": 209721876
         },     
-		"movie6": {
+		"movie7": {
             "head": "eric mcfadden",
             "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
             "vimeocode": 10347431
         },
-        "movie7": {
+        "movie8": {
             "head": "marner brown",
             "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4715443
         },
-        "movie8": {
+        "movie9": {
             "head": "charlotte marin",
             "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
             "vimeocode": 9802972
         },
-        "movie9": {
+        "movie10": {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
