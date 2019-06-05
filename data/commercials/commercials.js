a0c2541f6efd85aae9609ad8e7ba8ad1ca13255d
Xavier Raffin <xavier.raffin@gmail.com>
2019-06-05 17:54:11 +0200

    add spiderman
    spiderman

--- data/commercials/commercials.js
+++ data/commercials/commercials.js
@@ -1,25 +1,30 @@
 var videos = {
 	"movie1": {
-            "head": "lee cooper",
+            "head": "spiderman/eurorepar",
+            "desc": "spiderman / eurorepar<br>Director: Régis Raffin<br>Production: les improductibles<br>2019",
+            "vimeocode": 340438449
+        },
+	"movie2": {
+            "head": "innocent",
             "desc": "innocent, petits bonnets<br>Director: Régis Raffin<br>Production: les improductibles<br>2019",
             "vimeocode": 339991355
         },
-        "movie2": {
+        "movie3": {
             "head": "lee cooper",
             "desc": "the fine english denim company<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4689031
         },
-        "movie3": {
+        "movie4": {
             "head": "bruel/marin",
             "desc": "le concert<br>Director: Régis Raffin<br>Production: charlotte marin<br>2010",
             "vimeocode": 18087560
         },
-        "movie4": {
+        "movie5": {
             "head": "greenpeace",
             "desc": "ultimatum climatique<br>Director: Régis Raffin<br>Production: greenpeace<br>2009",
             "vimeocode": 9802411
         },
-        "movie5": {
+        "movie6": {
             "head": "france 4",
             "desc": "5 fruits et légumes par jours<br>Director: Régis raffin<br>production: eden<br>2011",
             "vimeocode": 82778494
