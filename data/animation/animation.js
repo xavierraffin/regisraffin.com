6366961e46d2408a3839ef53b4fdfedd3d450f1a
Xavier Raffin <xavier.raffin@gmail.com>
2019-07-01 12:12:43 +0200

    clandestino & new thumbs
    awesome stuff

--- data/animation/animation.js
+++ data/animation/animation.js
@@ -1,30 +1,35 @@
 var videos = {
-        "movie1": {
+		"movie1": {
+            "head": "manu chao",
+            "desc": "clandestino<br>Director: Régis Raffin<br>Production: Wise Bird<br>2019",
+            "vimeocode": 341833479
+        },
+        "movie2": {
             "head": "bnp paribas",
             "desc": "dream up<br>Director: Régis Raffin<br>Production: Angie Lab<br>2015",
             "vimeocode": 139208396
         },
-        "movie2": {
+        "movie3": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>fighting in the arena<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82081351
         },
-        "movie3": {
+        "movie4": {
             "head": "Louis Vuitton",
             "desc": "the little groom in<br>duel in the dust<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82103917
         },
-        "movie4": {
+        "movie5": {
             "head": "cinema",
             "desc": "main title<br>la confrere des larmes<br>Director: Régis raffin<br>Production: Maneki Lab / Full house<br>2013",
             "vimeocode": 82281952
         },
-        "movie5": {
+        "movie6": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>swinging in the clouds<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 82103920
         },
-        "movie6": {
+        "movie7": {
             "head": "louis vuitton",
             "desc": "red<br>journey<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 164057170
--- data/music-videos/music-videos.js
+++ data/music-videos/music-videos.js
@@ -1,50 +1,55 @@
 var videos = {
 			"movie1": {
+            "head": "manu chao",
+            "desc": "clandestino<br>Director: Régis Raffin<br>Production: Wise Bird<br>2019",
+            "vimeocode": 341833479
+        },
+			"movie2": {
             "head": "pershanoush",
-            "desc": "mesmerizing<br>Director: Régis Raffin<br>Production: Gunsmoke<br>2016",
+            "desc": "mesmerizing<br>Director: Régis Raffin<br>Production: Gunsmoke<br>2018",
             "vimeocode": 262004148
         },
-             "movie2": {
+             "movie3": {
             "head": "ror",
             "desc": "veggies can kill<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
             "vimeocode": 161273783
         },
-         "movie3": {
+         "movie4": {
             "head": "making of",
             "desc": "making of<br>Director: Régis Raffin<br>Production: Heroes<br>2016",
             "vimeocode": 163225802
         },
-        "movie4": {
+        "movie5": {
             "head": "ror",
             "desc": "hipsters must die<br>Director: Régis Raffin<br>Production: Feerkins Prod<br>2014",
             "vimeocode": 105849120
         },
-        	"movie5": {
+        	"movie6": {
             "head": "archifau",
             "desc": "chirac<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2017",
             "vimeocode": 209723372
         },
-         "movie6": {
+         "movie7": {
             "head": "archifau",
             "desc": "fillon<br>Director: Régis Raffin<br>Production: Flab/Canal+<br>2016",
             "vimeocode": 209721876
         },     
-		"movie7": {
+		"movie8": {
             "head": "eric mcfadden",
             "desc": "where is ferdinand ?<br>Director: Régis Raffin<br>Production: Bad reputation<br>2010",
             "vimeocode": 10347431
         },
-        "movie8": {
+        "movie9": {
             "head": "marner brown",
             "desc": "sun don't shine<br>Director: Régis Raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4715443
         },
-        "movie9": {
+        "movie10": {
             "head": "charlotte marin",
             "desc": "le plus beau jour de ma vie<br>Director: Régis Raffin<br>Production: charlotte marin<br>2012",
             "vimeocode": 9802972
         },
-        "movie10": {
+        "movie11": {
             "head": "kish mauve",
             "desc": "matthew<br>Director: Régis raffin<br>Production: lee cooper<br>2009",
             "vimeocode": 4683162
