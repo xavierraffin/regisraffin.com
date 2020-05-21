19ba228a391d6e8897256afffc1afccab8b651d1
Xavier Raffin <xavier.raffin@gmail.com>
2020-05-21 10:40:03 +0200

    heroes
    heroes

--- data/animation/animation.js
+++ data/animation/animation.js
@@ -1,35 +1,40 @@
 var videos = {
-		"movie1": {
+	"movie1": {
+            "head": "heroes in lockdown",
+            "desc": "Super héros en confinement<br>Director: Régis Raffin<br>Production: Bonkers<br>2020",
+            "vimeocode": 413920699
+        },
+		"movie2": {
             "head": "manu chao",
             "desc": "clandestino<br>Director: Régis Raffin<br>Production: Wise Bird<br>2019",
             "vimeocode": 341833479
         },
-        "movie2": {
+        "movie3": {
             "head": "bnp paribas",
             "desc": "dream up<br>Director: Régis Raffin<br>Production: Angie Lab<br>2015",
             "vimeocode": 139208396
         },
-        "movie3": {
+        "movie4": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>fighting in the arena<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82081351
         },
-        "movie4": {
+        "movie5": {
             "head": "Louis Vuitton",
             "desc": "the little groom in<br>duel in the dust<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82103917
         },
-        "movie5": {
+        "movie6": {
             "head": "cinema",
             "desc": "main title<br>la confrere des larmes<br>Director: Régis raffin<br>Production: Maneki Lab / Full house<br>2013",
             "vimeocode": 82281952
         },
-        "movie6": {
+        "movie7": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>swinging in the clouds<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 82103920
         },
-        "movie7": {
+        "movie8": {
             "head": "louis vuitton",
             "desc": "red<br>journey<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 164057170
