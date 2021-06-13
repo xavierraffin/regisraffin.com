1476a1e9f9b915f4daf76d7146e8ed53c62ae257
Xavier Raffin <81416072+regisraffin@users.noreply.github.com>
2021-06-13 21:04:54 +0200

    here we go
    2021

--- data/animation/animation.js
+++ data/animation/animation.js
@@ -5,55 +5,61 @@ var videos = {
             "vimeocode": 437171746
         },
         "movie2": {
+            "head": "come on 2021",
+            "desc": "Happy new year 2021<br>Director: Régis Raffin<br>2021",
+            "vimeocode": 496656781
+        },
+        "movie3": {
             "head": "heroes in lockdown",
             "desc": "Super héros en confinement<br>Director: Régis Raffin<br>Production: Bonkers<br>2020",
             "vimeocode": 413920699
         },
-		"movie3": {
+		"movie4": {
             "head": "manu chao",
             "desc": "clandestino<br>Director: Régis Raffin<br>Production: Wise Bird<br>2019",
             "vimeocode": 341833479
         },
-        "movie4": {
+        "movie5": {
             "head": "bnp paribas",
             "desc": "dream up<br>Director: Régis Raffin<br>Production: Angie Lab<br>2015",
             "vimeocode": 139208396
         },
-        "movie5": {
+        "movie6": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>fighting in the arena<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82081351
         },
-        "movie6": {
+        "movie7": {
             "head": "Louis Vuitton",
             "desc": "the little groom in<br>duel in the dust<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2012",
             "vimeocode": 82103917
         },
-        "movie7": {
+        "movie8": {
             "head": "Crazy Fingers",
             "desc": "crazy fingers<br>the race<br>Director: Régis raffin<br>Production: Maneki Lab<br>2013",
             "vimeocode": 410203984
         },
-        "movie8": {
+        "movie9": {
             "head": "Crazy Fingers",
             "desc": "crazy fingers<br>the adventurer<br>Director: Régis raffin<br>Production: Maneki Lab<br>2013",
             "vimeocode": 410203289
         },
-        "movie9": {
+        "movie10": {
             "head": "cinema",
             "desc": "main title<br>la confrere des larmes<br>Director: Régis raffin<br>Production: Maneki Lab / Full house<br>2013",
             "vimeocode": 82281952
         },
-        "movie10": {
+        "movie11": {
             "head": "louis vuitton",
             "desc": "the little groom is<br>swinging in the clouds<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 82103920
         },
-        "movie11": {
+        "movie12": {
             "head": "louis vuitton",
             "desc": "red<br>journey<br>Director: Régis Raffin<br>Production: Maneki Lab<br>2011",
             "vimeocode": 164057170
         }
     };
+
 
 
